3470822c1f7587f6e91eb49e4e16e212ff9359cf
AhmadjonovSardor <ahmadjonovsardor056@gmail.com>
2025-06-18 13:37:07 +0500

    Modellar tugadi
    CarTaxi, CarDilevry, CarDeliveryType,Brand,CarTaxiType modellarini tugatdim

--- core/models/models_site_Sardor.py
+++ core/models/models_site_Sardor.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-# from .models_auth import User
 
 class Brand(models.Model):
     name = models.CharField(verbose_name='Mashina brandi',max_length=32)
@@ -18,20 +17,36 @@ class Driver(models.Model):
         return self.fio
 
 
+class CarDeliveryType(models.Model):
+    name = models.CharField(default=1,choices=[
+        (1,'Kichik yuk'),
+        (2,'Orta yuk'),
+        (3,'Katta yuk'),
+    ])
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
 class CarDelivery(models.Model):
-    name = models.CharField(verbose_name=_('Mashina turi'),max_length=32)
-    height = models.PositiveSmallIntegerField(verbose_name=_('Mashina balandligi'))
-    width = models.PositiveSmallIntegerField(verbose_name=_('Mashina eni'))
-    length = models.PositiveSmallIntegerField(verbose_name=_('Mashina uzunligi'))
-    image = models.ImageField(verbose_name=_('Mashina rasmi'),upload_to='Car/image/')
+    name = models.CharField(verbose_name=_('Yuk mashina turi'),max_length=32)
+    height = models.PositiveSmallIntegerField(verbose_name=_('Yuk mashina balandligi'))
+    width = models.PositiveSmallIntegerField(verbose_name=_('Yuk mashina eni'))
+    length = models.PositiveSmallIntegerField(verbose_name=_('Yuk mashina uzunligi'))
+    image = models.ImageField(verbose_name=_('Yuk mashina rasmi'),upload_to='Car/image/')
     max_weight = models.PositiveSmallIntegerField(verbose_name=_('Qancha yuk kotara oladi?'))
-    number = models.CharField(verbose_name=_('Mashina raqami'), max_length=10)
+    number = models.CharField(verbose_name=_('Yuk mashina raqami'), max_length=10)
     price_km = models.PositiveSmallIntegerField(verbose_name='1 km uchun narx')
     created = models.DateTimeField(auto_now_add=True)
     brand = models.ForeignKey(Brand,on_delete=models.CASCADE,related_name='carDelivery_brand')
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='carDelivery_brand')
+    type = models.ForeignKey(CarDeliveryType, on_delete=models.CASCADE, related_name='carDelivery_type')
 
-
+    def __str__(self):
+        return self.name
 
 class CarTaxiType(models.Model):
     name = models.CharField(default=1,choices=[
@@ -45,6 +60,14 @@ class CarTaxiType(models.Model):
 
 
 class CarTaxi(models.Model):
+    name = models.CharField(verbose_name='Mashina nomi',max_length=32)
+    number = models.CharField(verbose_name='Mashina raqami',max_length=10)
+    date = models.DateTimeField(verbose_name='Mashina bazaga qoshilgan vaqt',auto_now_add=True)
+
     brand = models.ForeignKey(Brand,on_delete=models.CASCADE,related_name='carTaxi_brand')
     driver = models.ForeignKey(Driver,on_delete=models.CASCADE,related_name='carTaxi_brand')
     type = models.ForeignKey(CarTaxiType,on_delete=models.CASCADE,related_name='carTaxi_type')
+
+    def __str__(self):
+        return self.name
+
--- core/site/views_site_Sardor.py
+++ core/site/views_site_Sardor.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,redirect
+from core.models.models_site_Sardor import CarDelivery,CarTaxi,CarDeliveryType,CarTaxiType
+
 
 def delivery(request):
+
     ctx = {}
     return render(request,'site/delivery-details.html',ctx)
+
+
 
 def taxi(request):
     ctx = {}
